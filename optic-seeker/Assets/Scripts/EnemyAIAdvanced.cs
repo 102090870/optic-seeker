8e7e14cb98155a000148dace35de37a98bc3e71b
Leng700 <111410588+Leng700@users.noreply.github.com>
2022-10-12 16:13:53 +1100

    EnemyAiFixed

--- optic-seeker/Assets/Scripts/EnemyAIAdvanced.cs
+++ optic-seeker/Assets/Scripts/EnemyAIAdvanced.cs
@@ -12,85 +12,78 @@ public class EnemyAIAdvanced : MonoBehaviour
     public Transform travelP2;
     public Transform travelP3;
 
-    private float turnorder = 0f;
+    public float turnorder = 0f;
 
 
     public LayerMask whatIsGround;
 
 
     //Patroling
+    public Vector3 walkPoint1;
     public Vector3 walkPoint2;
-    bool walkPointSet2;
+    public Vector3 walkPoint3;
+    public bool walkPointSet;
 
+    private void Start()
+    {
+        walkPointSet = true;
+    }
     private void Awake()
     {
         player = GameObject.Find("Character").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
+    private void Update()
+    {
+        if(turnorder >= 3f)
+        {
+            turnorder = 0;
+        }
+    }
+
     public void Patroling2()
     {
-        if (turnorder < 1f)
-        {
-            if (!walkPointSet2) SearchWalkPoint1();
-
-            if (walkPointSet2)
-                agent.SetDestination(walkPoint2);
-
-            Vector3 distanceToWalkPoint = transform.position - walkPoint2;
-
-            if (distanceToWalkPoint.magnitude < 1f)
-                walkPointSet2 = false;
-        }
-
-        if (turnorder < 1f)
-        {
-            if (!walkPointSet2) SearchWalkPoint2();
-
-            if (walkPointSet2)
-                agent.SetDestination(walkPoint2);
-
-            Vector3 distanceToWalkPoint = transform.position - walkPoint2;
-
-            if (distanceToWalkPoint.magnitude < 1f)
-                walkPointSet2 = false;
-        }
-
-        if (turnorder < 1f)
-        {
-            if (!walkPointSet2) SearchWalkPoint3();
-
-            if (walkPointSet2)
-                agent.SetDestination(walkPoint2);
-
-            Vector3 distanceToWalkPoint = transform.position - walkPoint2;
-
-            if (distanceToWalkPoint.magnitude < 1f)
-                walkPointSet2 = false;
-        }
-    }
-
-    private void SearchWalkPoint1()
-    {
-
-        walkPoint2 = new Vector3(travelP1.position.x, transform.position.y, travelP1.position.z);
-        if (Physics.Raycast(walkPoint2, -transform.up, 2f, whatIsGround))
-            walkPointSet2 = true;
-    }
-
-    private void SearchWalkPoint2()
-    {
-
+        walkPoint1 = new Vector3(travelP1.position.x, transform.position.y, travelP1.position.z);
         walkPoint2 = new Vector3(travelP2.position.x, transform.position.y, travelP2.position.z);
-        if (Physics.Raycast(walkPoint2, -transform.up, 2f, whatIsGround))
-            walkPointSet2 = true;
-    }
-    private void SearchWalkPoint3()
-    {
+        walkPoint3 = new Vector3(travelP3.position.x, transform.position.y, travelP3.position.z);
 
-        walkPoint2 = new Vector3(travelP3.position.x, transform.position.y, travelP3.position.z);
-        if (Physics.Raycast(walkPoint2, -transform.up, 2f, whatIsGround))
-            walkPointSet2 = true;
+
+        if (turnorder < 1f && turnorder >= 0f)
+        {
+            agent.SetDestination(walkPoint1);
+
+            Vector3 distanceToWalkPoint = transform.position - walkPoint1;
+
+            if (distanceToWalkPoint.magnitude < 1f)
+            {
+                turnorder++;
+            }
+        }
+
+        if (turnorder < 2f && turnorder >= 1f)
+        {
+            agent.SetDestination(walkPoint2);
+
+            Vector3 distanceToWalkPoint = transform.position - walkPoint2;
+
+            if (distanceToWalkPoint.magnitude < 1f)
+            {
+                turnorder++;
+            }
+        }
+
+        if (turnorder < 3f && turnorder >= 2f)
+        {
+            agent.SetDestination(walkPoint1);
+
+            Vector3 distanceToWalkPoint = transform.position - walkPoint1;
+
+            if (distanceToWalkPoint.magnitude < 1f)
+            {
+                turnorder++;
+            }
+        }
     }
 
     public void ChasePlayer()
